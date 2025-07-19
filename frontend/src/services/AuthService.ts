05ec6304cb293f85d5bc6a3edf55855b3469196e
AJ Barea <ajbareaa@gmail.com>
2025-07-18 23:43:36 -0400

    fix(auth): update API_URL to include full localhost path

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -1,7 +1,7 @@
 import type { RegisterRequest } from '@/types/RegisterRequest';
 import axios from 'axios';
 
-const API_URL = '/api/auth/';
+const API_URL = 'http://localhost:8080/api/auth/';
 
 class AuthService {
     register(user: RegisterRequest) {
